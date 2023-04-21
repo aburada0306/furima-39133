3caf90a032c335cdd1d5a12685b0d99b26323f1f
aburada0306 <128586717+aburada0306@users.noreply.github.com>
2023-04-21 16:48:52 +0900

    ユーザー管理機能-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   validates :name,:chinese_first,:chinese_last,:kana_first,:kana_last,:birth, presence: true
+         
+   validates :name,:birth,:chinese_first,:chinese_last,:kana_first,:kana_last, presence: true
 
-   validates :kana_first,:kana_last,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
-   validates :chinese_first,:chinese_last,format: { with: /\A[一-龥]+\z/}
-   
+   validates :kana_first,:kana_last,format: {with: /\A[ァ-ヶー－]+\z/, message: "Input full-width katakana characters"},allow_blank:true
+   validates :chinese_first,:chinese_last,format: {with: /\A[ぁ-んァ-ン一-龥]/, message: "Input full-width characters"},allow_blank:true
+   validates :password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
 
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,112 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+before do
+  @user = FactoryBot.build(:user)
+end
+
+
+  describe 'ユーザー新規登録' do
+    
+    it 'nameが空では登録できない' do
+      @user.name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Name can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email can't be blank"
+    end
+    it 'emailが@なしでは登録できない' do
+      @user.email = 'abujun.email.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email is invalid"
+    end
+
+
+    it '同じemailが登録済みである場合、登録できない' do
+      @user.save
+      @user2 = FactoryBot.build(:user)
+      @user2.email = @user.email 
+      @user2.valid?
+
+      expect(@user2.errors.full_messages).to include "Email has already been taken"
+    end
+
+
+    it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password can't be blank"
+    end
+    it 'passwordが英数字混合でないと登録できない' do
+      @user.password = '123456'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password is invalid"
+    end
+    it 'passwordが6文字以上でないと登録できない' do
+      @user.password = '1234a'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Password is invalid"
+    end
+    it 'passwordと確認用のpasswordが一致していないと登録できない' do
+      @user.password = '123456'
+      @user.password_confirmation = '1234567'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'Chinese_firstが空では登録できない' do
+      @user.chinese_first = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Chinese first can't be blank"
+    end
+    it 'Chinese_firstが全角のひらがな/カナ/漢字以外では登録できない' do
+      @user.chinese_first = 'abu'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Chinese first Input full-width characters"
+    end
+
+    it 'chinese_lastが空では登録できない' do
+      @user.chinese_last = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Chinese last can't be blank"
+    end
+    it 'chinese_lastが全角のひらがな/カナ/漢字以外では登録できない' do
+      @user.chinese_last = 'jun'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Chinese last Input full-width characters"
+    end
+
+    it 'Kana_firstが空では登録できない' do
+      @user.kana_first = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Kana first can't be blank"
+    end
+    it 'Kana_firstが全角のカナ以外では登録できない' do
+      @user.kana_first = 'abu'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Kana first Input full-width katakana characters"
+    end
+
+    it 'kana_lastが空では登録できない' do
+      @user.kana_last = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Kana last can't be blank"
+    end
+    it 'kana_lastが全角のカナ以外では登録できない' do
+      @user.kana_last = 'jun'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Kana last Input full-width katakana characters"
+    end
+
+    it 'Birthが空では登録できない' do
+      @user.birth = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Birth can't be blank"
+    end
+  end
+
+end
