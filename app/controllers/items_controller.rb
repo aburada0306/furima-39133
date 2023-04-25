c7472b0af1a8816150b609161b95faa1e9b43f2a
aburada0306 <128586717+aburada0306@users.noreply.github.com>
2023-04-25 21:59:41 +0900

    商品一覧表示機能-2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.all.order(id: "DESC")
+    @items = Item.includes(:user).order("created_at DESC")
   end
   def new
     @item = Item.new
